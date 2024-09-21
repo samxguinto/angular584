b56696a680229b716f73fc1abab09f7015d45808
samxguinto <spamguinto@gmail.com>
2024-09-20 17:01:31 -0700

    nav up

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon'
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatIconModule, MatToolbar],
+  imports: [RouterOutlet, RouterLink, MatIconModule, MatToolbar, MatButtonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
