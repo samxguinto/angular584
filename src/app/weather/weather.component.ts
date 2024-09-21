8f1b7cb14fa095e1734a6c3bf9a96e6af15011be
samxguinto <spamguinto@gmail.com>
2024-09-20 17:25:43 -0700

    linked url into variable

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 interface WeatherForecast {
   date: string;
   temperatureC: number;
@@ -23,7 +24,7 @@ export class WeatherComponent implements OnInit {
   }
 
   getForecasts() {
-    this.http.get<WeatherForecast[]>('http://localhost:5116/weatherforecast').subscribe(
+    this.http.get<WeatherForecast[]>(`${environment.baseUrl}weatherforecast`).subscribe(
       (result) => {
         this.forecasts = result;
       },
